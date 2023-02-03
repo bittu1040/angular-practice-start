6b3a40bb43a7876ba1006e54e81a4ca2d9353a14
bittu1040 <32195733+bittu1040@users.noreply.github.com>
2023-02-03 10:48:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-practice-start
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-practice-start-tadv67)
---
+++ app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+import {PassengerI} from './passenger';
+
+@Component({
+  selector:'app-root',
+  templateUrl:'./app.component.html',
+  styleUrls:['app.component.css']
+})
+export class AppComponent {
+
+ passengers: PassengerI[] = [{
+   id:1,
+   fullname:'krati',
+   checkedIn:true
+
+ }, 
+ {
+   id:2,
+   fullname:'bittu',
+   checkedIn: false
+ }
+   
+ ]
+ 
+ 
+  }
